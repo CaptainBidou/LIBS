92104a1604864c678164ef00ffc7693a9f689916
Isaias <isaias.97.ib@gmail.com>
2024-07-17 15:06:07 -0300

    ekf with more decimal place

--- LIBS_Back/Models/EKF.py
+++ LIBS_Back/Models/EKF.py
@@ -15,10 +15,10 @@ import graph
 ##                   G L O B A L   V A R I A B L E S             ##
 ###################################################################
 SAMPLING_RATE = 1  # seconds
-R1 = 9.818*10**-3
-C1 = 15.639*10**3
-R2 = 31.463*10**-3
-C2 = 39.332*10**3
+R1 = 0.009818410271493
+C1 = 1.563954740330107*10**4
+R2 = 0.031463057438081
+C2 = 3.933292323912280*10**4
 Qn = 3.08
 ###################################################################
 ##            F U N C T I O N    D E C L A R A T I O N           ##
@@ -32,11 +32,12 @@ Qn = 3.08
 class EKF:
     def __init__(self,fileToOpen,mode):
         self.fileToOpen = fileToOpen
-        self.x = np.array([[0],[0],[1]]) # state vector
+        self.x = np.array([[0],[0],[0.5]]) # state vector
         if mode == 1:
-            self.P = np.diag([10**-3, 10**-3, 10 ** -1])  # covariance matrix
-            self.Q = 10 ** -6 * np.eye(3)  # process noise
+            self.P = np.diag([0.001, 0.001, 0.01])  # covariance matrix
+            self.Q = 0.000001 * np.eye(3)  # process noise
             self.R = 0.2 # measurement noise
+
         # elif mode == 2:
         #     self.P = np.diag([1*10**-3, 1*10**-3, 10])
         #     self.Q = np.diag([1 * 10 ** -5, 1 * 10 ** -5, 1 * 10 ** -9])  # process noise
@@ -51,14 +52,15 @@ class EKF:
         #     self.R = 10**-5  # measurement noise
 
         self.A = np.array([[1-(SAMPLING_RATE/(R1*C1)), 0, 0], [0, 1-(SAMPLING_RATE/(R2*C2)), 0], [0, 0,1]])# non linear function based on the chosen model see the paper p2
-        self.B = np.array([[SAMPLING_RATE/C1], [SAMPLING_RATE/C2], [SAMPLING_RATE/(3600*Qn)]]) # see the paper p 2
+        self.B = np.array([[SAMPLING_RATE/C1], [SAMPLING_RATE/C2], [-SAMPLING_RATE/(3600*Qn)]]) # see the paper p 2
         self.u = 0 # current
         self.K = None  # Kalman gain
         self.z = 0  # voltage
         self.step = 0
         self.safeX = []
+        self.P = np.dot(np.dot(self.A, self.P), np.transpose(self.A)) + self.Q # extrapolated covariance matrix
         if fileToOpen is not None :
-            with open('C:/Users/tjasr/Documents/GitHub/LIBS/LIBS_Back/datasets/'+fileToOpen, 'r') as file:
+            with open('C:/LIBS/LIBS_Back/dataTest/'+fileToOpen, 'r') as file:
                 self.data = pd.read_csv(file, delimiter=';')
         else:
             self.data = pd.DataFrame()# 2 columns time and voltage
@@ -69,15 +71,15 @@ class EKF:
 
     # voc 
     def h(self,x):
-        return (10**4)*((x**9)*0.140437+(x**8)*-0.693293+(x**7)*1.448317+(x ** 6) * -1.665299 + (x ** 5) * 1.148704 + (x ** 4) * -0.486836 + (x ** 3) * 0.125421 + (x ** 2) * -0.01891 + x * 0.001657 + 0.000269)
+        return (10**4)*((x**9)*0.140437566214432+(x**8)*-0.693293580320924+(x**7)*1.448317451181394 + (x ** 6) *-1.665299094951629 + (x ** 5)*1.148704101226141 + (x ** 4)*-0.486836353839831 + (x ** 3)*0.125420712206318 + (x ** 2)*-0.018961803736654 + x*0.001657801378501 + 0.000269333059573)
     def h_derivative(self,x):
-        return (10**4)*( 9*(x**8)*0.140437+8*(x**7)*-0.693293+7*(x**6)*1.448317+6*(x ** 5) * -1.665299 + 5*(x ** 4) * 1.148704 + 4*(x ** 3) * -0.486836 + 3*(x ** 2) * 0.125421 + 2*x * -0.01891 + 0.001657)
+        return (10**4)*( 9*(x**8)*0.140437566214432 + 8*(x**7)*-0.693293580320924 + 7*(x**6)*1.448317451181394 + 6*(x ** 5)*-1.665299094951629 + 5*(x ** 4)*1.148704101226141 + 4*(x ** 3)*-0.486836353839831 + 3*(x ** 2)*0.125420712206318 + 2*x*-0.018961803736654 + 0.001657801378501)
 
     def R0(self,x):
-        return (10**3)*((x**9)*0.440568+(x**8)*-2.188575+(x**7)*4.662025+(x**6)*-5.561277+(x**5)*4.069003+(x**4)*-1.878727+(x**3)*0.541296+(x**2)*-0.092097+x*0.008056-0.000161)
+        return (10**3)*((x**9)*0.440568380336331+(x**8)*-2.188575118770938+(x**7)*4.662025929324535+(x**6)*-5.561277160719505+(x**5)*4.069003040512039+(x**4)*-1.878727644202677+(x**3)*0.541295950462937+(x**2)*-0.092097275963785+x*0.008056926448651-0.000160671690337)
 
     def R0_derivative(self,x):
-        return (10**3)*(9*(x**8)*0.440568+8*(x**7)*-2.188575+7*(x**6)*4.662025+6*(x**5)*-5.561277+5*(x**4)*4.069003+4*(x**3)*-1.878727+3*(x**2)*0.541296+2*x*-0.092097+0.008056)
+        return (10**3)*(9*(x**8)*0.440568380336331+8*(x**7)*-2.188575118770938+7*(x**6)*4.662025929324535+6*(x**5)*-5.561277160719505+5*(x**4)*4.069003040512039+4*(x**3)*-1.878727644202677+3*(x**2)*0.541295950462937+2*x*-0.092097275963785+0.008056926448651)
 
     def g(self,x0,x1,x2,u):
         return - x0 - x1 - self.R0(x2) * u + self.h(x2)
@@ -93,18 +95,21 @@ class EKF:
 
         # Compute P(k+1)
         self.P = np.dot(np.dot(self.A, self.P), np.transpose(self.A)) + self.Q
+        # print(self.P)
 
     def correction(self):
         # Compute K(k+1)
 
         number = np.dot(np.dot(self.dg(), self.P), np.transpose(self.dg())) + self.R
-        if number[0][0] > 7*10**302:
-            number[0][0] = 0
-        else :
-            number[0][0] = 1/number[0][0]
+        # if number[0][0] > 7*10**302:
+        #     number[0][0] = 0
+        # else :
+        number[0][0] = 1/number[0][0]
 
-        self.K = (number[0][0]) * np.dot(self.P, np.transpose(self.dg()))
-        self.K.shape = (3,1)
+        self.K =  np.dot(self.P, np.transpose(self.dg()))/(np.dot(np.dot(self.dg(), self.P), np.transpose(self.dg())) + self.R)
+        # print(self.K)
+        # self.K.shape = (3,1)
+        # print(self.K)
 
         # Compute x(k+1)
         self.x = self.x + self.K * (self.z - self.g(self.x[0][0],self.x[1][0],self.x[2][0],self.u))
@@ -230,8 +235,8 @@ class EKF:
 
 
 import os
-for root, dirs, files in os.walk("C:/Users/tjasr/Documents/GitHub/LIBS/LIBS_Back/datasets/Random/"):
+for root, dirs, files in os.walk("C:/LIBS/LIBS_Back/dataTest/"):
     for file in files:
         if file.endswith(".txt"):
-            ekf = EKF("Random/"+file, 1)
+            ekf = EKF(file, 1)
             ekf.runOffline()
