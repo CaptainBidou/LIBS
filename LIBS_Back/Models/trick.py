855e5867573bc03726e137cae22f8b5e4e0b4c85
CaptainBidou <98890160+CaptainBidou@users.noreply.github.com>
2024-07-23 14:04:36 -0300

    Delete LIBS_Back directory

--- LIBS_Back/Models/trick.py
+++
@@ -1,8 +0,0 @@
-import os
-import EKF
-
-for root, dirs, files in os.walk("C:/Users/tjasr/Documents/GitHub/LIBS/LIBS_Back/datasets/Random/"):
-    for file in files:
-        if file.endswith(".txt"):
-            ekf = EKF("Random/"+file, 1)
-            ekf.runOffline()
