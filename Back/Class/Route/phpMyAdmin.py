6cdb63b68a7b1c669d3d0faa440ee28ace3ae619
CaptainBidou <tomas.salvado_robalo@ig2i.centralelille.fr>
2024-09-30 14:44:41 -0300

    php status

--- Back/Class/Route/phpMyAdmin.py
+++ Back/Class/Route/phpMyAdmin.py
@@ -27,3 +27,18 @@ def requestInsert(sql,data):
     mydb.commit()
     mydb.close()
     return mycursor.lastrowid
+
+def getStatus():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
+    mycursor = mydb.cursor()
+    # verif that libs exists
+    mycursor.execute("SHOW DATABASES")
+    databases = mycursor.fetchall()
+    for database in databases:
+        if database[0] == "libs":
+            return True
+    return False
